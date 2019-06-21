bfad4370913575ee2dbc447488d358073c3bc086
Suhaas <suhaas.shekar@gmail.com>
2019-06-21 20:33:05 +0100

    CUDA commit after hell lot of installations

---
+++ main.py
@@ -0,0 +1,15 @@
+#
+# Suhaas Shekar
+# 118220499
+
+import torch
+import numpy as np
+
+# check if CUDA is available
+train_on_gpu = torch.cuda.is_available()
+
+if not train_on_gpu:
+    print('CUDA is not available.  Training on CPU ...')
+else:
+    print('CUDA is available!  Training on GPU ...')
+
