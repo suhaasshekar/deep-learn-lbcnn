17aa108bf1de0430d6025cf11c76a9b5a3fbbc57
Suhaas <suhaas.shekar@gmail.com>
2019-07-15 17:05:23 +0100

    First meaningful run, 100 epochs 2 LBC layers 5k images 0.0001 lr 38% rain test accuracy

--- lbcnn.py
+++ lbcnn.py
@@ -130,48 +130,38 @@ class Net(nn.Module):
         self.layer1 = nn.Conv2d(3, 8, 3, stride=1, padding=1, bias=False)
         self.layer1.weight = nn.Parameter(self.weights, requires_grad=False)
 
-        self.layer2 = nn.ReLU()
+        self.layer2 = nn.Conv2d(8, 1, 1, stride=1, padding=0)
 
-        self.layer3 = nn.Conv2d(8, 1, 1, stride=1, padding=0)
+        self.layer3 = nn.Conv2d(1, 8, 3, stride=1, padding=1, bias=False)
+        self.layer3.weight = nn.Parameter(self.weights1, requires_grad=False)
 
-        self.layer4 = nn.Conv2d(1, 8, 3, stride=1, padding=1, bias=False)
-        self.layer4.weight = nn.Parameter(self.weights1, requires_grad=False)
-
-        self.first_lbc_layer = nn.Sequential(self.layer1, self.layer2, self.layer3)
-        self.lbc_layers = nn.Sequential(self.layer4, self.layer2, self.layer3)
+        self.layer4 = nn.Conv2d(8, 1, 1, stride=1, padding=0)
 
         #self.pool = nn.AvgPool2d((930, 1250), stride=6)
         self.pool = nn.AvgPool2d((226, 226), stride=6)
-
+        #self.pool = nn.MaxPool2d((226, 226), stride=6)
         self.fc1 = nn.Linear(36, 256)
         self.fc2 = nn.Linear(256, 64)
         self.fc3 = nn.Linear(64, 2)
         self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
-        x = self.first_lbc_layer(x)
 
-        #x = self.layer4(x)
-        #x = self.layer2(x)
-        #x = self.layer3(x)
-
-
-        for i in range(10):
-            x = self.lbc_layers(x)
-
+        x = F.relu(self.layer1(x))
+        x = self.layer2(x)
+        x = F.relu(self.layer3(x))
+        x = self.layer4(x)
         x = self.pool(x)
-
         #x = x.view(-1, 1228800)
         x = x.view(-1, 36)
-
         x = F.relu(self.fc1(x))
         x = self.dropout(F.relu(self.fc2(x)))
         x = self.fc3(x)
 
         return x
 
-# model = Net()
-# print(model)
+#model = Net()
+#print(model)
 # print(model.layer1.weight)
 # print(model.layer1.weight.shape)
 # print(model.layer3.weight)
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import torch.nn as nn
 import torchvision
 import numpy as np
 import torchvision.transforms as transforms
-from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.sampler import SubsetRandomSampler, WeightedRandomSampler
 from pathlib import Path
-from torch.utils.data import DataLoader, Sampler
+from torch.utils.data import DataLoader, Subset
 from torchvision import datasets
 from PIL import Image
 from colour_demosaicing import demosaicing_CFA_Bayer_bilinear as demosaic
@@ -23,6 +23,13 @@ def load_image(image_path):
     img = demosaic(img, 'gbrg')
     im = Image.fromarray(np.uint8(img))
     return im
+
+def sampler_(labels):
+    counts = np.array([len(np.where(labels == t)[0]) for t in counts_array])
+    weights = torch.tensor([1.0 / x if x else 0 for x in counts], dtype=torch.float)
+    sample_weights = np.array([weights[t] for t in labels])
+    sampler = WeightedRandomSampler(sample_weights, len(sample_weights), replacement=True)
+    return sampler
 
 # check if CUDA is available
 train_on_gpu = torch.cuda.is_available()
@@ -51,32 +58,38 @@ DATA_PATH_TRAIN = Path("/home/ss20/dataset")
 DATA_PATH_TEST = Path("/home/ss20/sample-data")
 
 train_data = datasets.ImageFolder(root=DATA_PATH_TRAIN, transform=transform, loader=load_image)
-train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+# train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
 test_data = datasets.ImageFolder(root=DATA_PATH_TEST, transform=transform, loader=load_image)
 test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
 # obtain training indices that will be used for validation
+labels = [x[1] for x in train_data.samples]
+no_of_classes =  len(train_data.classes)
+counts_array = np.array([x for x in range(no_of_classes)])
 num_train = len(train_data)
 indices = list(range(num_train))
 np.random.shuffle(indices)
 split = int(np.floor(valid_size * num_train))
 #train_idx, valid_idx = indices[split:], indices[:split]
-train_idx, valid_idx = indices[:48000], indices[48000:60000]
-#print(split)
+train_idx, valid_idx = indices[:400], indices[400:500]
+train_labels = [labels[x] for x in train_idx]
+#print(train_labels)
+val_labels = [labels[x] for x in valid_idx]
+train_sampler, valid_sampler = sampler_(train_labels), sampler_(val_labels)
 
 # define samplers for obtaining training and validation batches
-train_sampler = SubsetRandomSampler(train_idx)
-valid_sampler = SubsetRandomSampler(valid_idx)
+# train_sampler = SubsetRandomSampler(train_idx)
+# valid_sampler = SubsetRandomSampler(valid_idx)
 
 # prepare data loaders (combine dataset and sampler)
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+train_loader = torch.utils.data.DataLoader(Subset(train_data, train_idx), batch_size=batch_size,
     sampler=train_sampler, num_workers=num_workers)
-valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+valid_loader = torch.utils.data.DataLoader(Subset(train_data, valid_idx), batch_size=batch_size,
     sampler=valid_sampler, num_workers=num_workers)
 
 # specify the image classes
-classes = ['rain', 'sunny']
+classes = train_data.classes
 
 # # helper function to un-normalize and display an image
 # def imshow(img):
@@ -115,10 +128,10 @@ if train_on_gpu:
 criterion = nn.CrossEntropyLoss()
 
 # specify optimizer
-optimizer = optim.SGD(model.parameters(), lr=0.01)
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
 # number of epochs to train the model
-n_epochs = 10
+n_epochs = 1
 
 valid_loss_min = np.Inf  # track change in validation loss
 
@@ -131,6 +144,8 @@ for epoch in range(1, n_epochs + 1):
     valid_loss = 0.0
     class_correct = list(0. for i in range(len(classes)))
     class_total = list(0. for i in range(len(classes)))
+    val_class_correct = list(0. for i in range(len(classes)))
+    val_class_total = list(0. for i in range(len(classes)))
 
     ###################
     # train the model #
@@ -138,9 +153,11 @@ for epoch in range(1, n_epochs + 1):
     model.train()
     print("Training the model..")
     outfile.write("Training the model..")
+    outfile.flush()
     counter = 0
     for data, target in train_loader:
         # move tensors to GPU if CUDA is available
+        #print(target)
         if train_on_gpu:
             data, target = data.cuda(), target.cuda()
         # clear the gradients of all optimized variables
@@ -168,6 +185,8 @@ for epoch in range(1, n_epochs + 1):
         #print("torch.max", torch.max(output, 1))
         # compare predictions to true label
         correct_tensor = pred.eq(target.data.view_as(pred))
+        #print(target)
+        #print(pred)
         #print("correct tensor, pred.eq", correct_tensor, target.data.view_as(pred))
         correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
         #print("correct", correct)
@@ -177,7 +196,7 @@ for epoch in range(1, n_epochs + 1):
             class_correct[label] += correct[i].item()
             class_total[label] += 1
 
-        del data, target
+        del data, target, pred, correct, correct_tensor
         torch.cuda.empty_cache()
         gc.collect()
 
@@ -196,7 +215,19 @@ for epoch in range(1, n_epochs + 1):
             loss = criterion(output, target)
             # update average validation loss
             valid_loss += loss.item() * data.size(0)
-            del data, target
+
+            # convert output probabilities to predicted class
+            _, pred = torch.max(output, 1)
+            # compare predictions to true label
+            correct_tensor = pred.eq(target.data.view_as(pred))
+            correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+            # calculate test accuracy for each object class
+            for i in range(batch_size):
+                label = target.data[i]
+                val_class_correct[label] += correct[i].item()
+                val_class_total[label] += 1
+
+            del data, target, pred, correct, correct_tensor
             torch.cuda.empty_cache()
             gc.collect()
 
@@ -209,6 +240,7 @@ for epoch in range(1, n_epochs + 1):
         epoch, train_loss, valid_loss))
     outfile.write('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
         epoch, train_loss, valid_loss))
+    outfile.flush()
 
     # save model if validation loss has decreased
     if valid_loss <= valid_loss_min:
@@ -218,10 +250,12 @@ for epoch in range(1, n_epochs + 1):
         outfile.write('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
             valid_loss_min,
             valid_loss))
+        outfile.flush()
         torch.save(model.state_dict(), 'model_cifar.pt')
         valid_loss_min = valid_loss
 
-    for i in range(len(classes)):
+    # Calculating Train Accuracy
+    for i in range(no_of_classes):
         if class_total[i] > 0:
             print('Train Accuracy of %5s: %2d%% (%2d/%2d)' % (
                 classes[i], 100 * class_correct[i] / class_total[i],
@@ -229,23 +263,52 @@ for epoch in range(1, n_epochs + 1):
             outfile.write('Train Accuracy of %5s: %2d%% (%2d/%2d)' % (
                 classes[i], 100 * class_correct[i] / class_total[i],
                 np.sum(class_correct[i]), np.sum(class_total[i])))
+            outfile.flush()
         else:
             print('Train Accuracy of %5s: N/A (no training examples)' % (classes[i]))
             outfile.write('Train Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+            outfile.flush()
+
+    # Calculating Validation Accuracy
+    for i in range(no_of_classes):
+        if val_class_total[i] > 0:
+            print('Validation Accuracy of %5s: %2d%% (%2d/%2d)' % (
+                classes[i], 100 * val_class_correct[i] / val_class_total[i],
+                np.sum(val_class_correct[i]), np.sum(val_class_total[i])))
+            outfile.write('Validation Accuracy of %5s: %2d%% (%2d/%2d)' % (
+                classes[i], 100 * val_class_correct[i] / val_class_total[i],
+                np.sum(val_class_correct[i]), np.sum(val_class_total[i])))
+            outfile.flush()
+        else:
+            print('Val Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+            outfile.write('Val Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+            outfile.flush()
+
 
     print('\nTrain Accuracy (Overall): %2d%% (%2d/%2d)\n\n' % (
         100. * np.sum(class_correct) / np.sum(class_total),
         np.sum(class_correct), np.sum(class_total)))
-    outfile.write('\nTrain Accuracy (Overall): %2d%% (%2d/%2d)\n\n' % (
+    outfile.write('\nTrain Accuracy (Overall): %2d%% (%2d/%2d)' % (
         100. * np.sum(class_correct) / np.sum(class_total),
         np.sum(class_correct), np.sum(class_total)))
+    outfile.flush()
 
+    print('Validation Accuracy (Overall): %2d%% (%2d/%2d)\n\n' % (
+        100. * np.sum(val_class_correct) / np.sum(val_class_total),
+        np.sum(val_class_correct), np.sum(val_class_total)))
+    outfile.write('Validation Accuracy (Overall): %2d%% (%2d/%2d)\n\n' % (
+        100. * np.sum(val_class_correct) / np.sum(val_class_total),
+        np.sum(val_class_correct), np.sum(val_class_total)))
+    outfile.flush()
 
 # track test loss
 test_loss = 0.0
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
+class_correct = list(0. for i in range(2))
+class_total = list(0. for i in range(2))
 
+model.load_state_dict(torch.load('model_cifar1.pt'))
+print('Load saved model for testing...')
+print('Testing the model..')
 model.eval()
 with torch.no_grad():
     # iterate over test data
@@ -274,8 +337,9 @@ with torch.no_grad():
 test_loss = test_loss/len(test_loader.dataset)
 print('Test Loss: {:.6f}\n'.format(test_loss))
 outfile.write('Test Loss: {:.6f}\n'.format(test_loss))
+outfile.flush()
 
-for i in range(2):
+for i in range(no_of_classes):
     if class_total[i] > 0:
         print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
             classes[i], 100 * class_correct[i] / class_total[i],
@@ -283,9 +347,11 @@ for i in range(2):
         outfile.write('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
             classes[i], 100 * class_correct[i] / class_total[i],
             np.sum(class_correct[i]), np.sum(class_total[i])))
+        outfile.flush()
     else:
         print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
         outfile.write('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+        outfile.flush()
 
 print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
     100. * np.sum(class_correct) / np.sum(class_total),
@@ -293,70 +359,6 @@ print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
 outfile.write('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
     100. * np.sum(class_correct) / np.sum(class_total),
     np.sum(class_correct), np.sum(class_total)))
+outfile.flush()
 
 outfile.close()
-
-
-# no_of_classes = 2
-#
-# def sampler_(labels):
-#     #_, counts = np.unique(labels, return_counts=True)
-#
-#     counts = np.array([len(np.where(labels == t)[0]) for t in np.array([0,1])])
-#     print(counts)
-#     weights = 1.0 / torch.tensor(counts, dtype=torch.float)
-#     print(weights)
-#     sample_weights = np.array([weights[t] for t in labels])
-#     #sample_weights = weights[labels]
-#     print(sample_weights)
-#     sampler = WeightedRandomSampler(sample_weights, len(sample_weights), replacement=True)
-#     return sampler
-#
-# # check if CUDA is available
-# train_on_gpu = torch.cuda.is_available()
-# #train_on_gpu = False
-#
-# if not train_on_gpu:
-#     print('CUDA is not available.  Training on CPU ...')
-# else:
-#     print('CUDA is available!  Training on GPU ...')
-#
-# # number of subprocesses to use for data loading
-# num_workers = 10
-# # how many samples per batch to load
-# batch_size = 100
-# # percentage of training set to use as validation
-# valid_size = 0.25
-#
-# # convert data to a normalized torch.FloatTensor
-# transform = transforms.Compose([
-#     transforms.Resize((256,256)),
-#     transforms.ToTensor(),
-#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-#     ])
-#
-# DATA_PATH_TRAIN = Path("/home/ss20/bla-data")
-#
-#
-# train_data = datasets.ImageFolder(root=DATA_PATH_TRAIN, transform=transform, loader=load_image)
-#
-# # obtain training indices that will be used for validation
-# labels = [x[1] for x in train_data.samples]
-# num_train = len(train_data)
-# indices = list(range(num_train))
-# np.random.shuffle(indices)
-# split = int(np.floor(valid_size * num_train))
-# train_idx, valid_idx = indices[split:], indices[:split]
-# train_labels = [labels[x] for x in train_idx]
-# val_labels = [labels[x] for x in valid_idx]
-# print(train_labels, val_labels)
-# train_sampler, valid_sampler = sampler_(train_labels), sampler_(val_labels)
-#
-#
-#
-#
-# # prepare data loaders (combine dataset and sampler)
-# train_loader = torch.utils.data.DataLoader(Subset(train_data, train_idx), batch_size=batch_size,
-#     sampler=train_sampler, num_workers=num_workers)
-# valid_loader = torch.utils.data.DataLoader(Subset(train_data, valid_idx), batch_size=batch_size,
-#     sampler=valid_sampler, num_workers=num_workers)
